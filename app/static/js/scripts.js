86a762fc59294b912458688ae116245be97f6519
Troppo99 <troppolungo99@gmail.com>
2024-12-11 10:50:07 +0700

    video muncul

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,12 +1,12 @@
 // toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#hamburger-menu").onclick = () => {
+const hamburger = document.querySelector("#hamburger-report"); // Sesuaikan ID
+
+hamburger.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
 // klik untuk hide sidebar
-const hamburger = document.querySelector("#hamburger-menu");
-
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
